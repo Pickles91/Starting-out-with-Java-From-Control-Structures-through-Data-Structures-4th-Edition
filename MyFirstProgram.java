e42deb80690a952f70da6bfe5d098be89e3071b0
Steven Sommer <stevensommer91@gmail.com>
2021-09-19 02:15:20 -0500

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,19 @@
+/* Programmer: Steven Sommer
+ * Date: 9/19/2021
+ * Book: starting out with>>> JAVA From Control Structures through Data Structures 
+ * Edition: 4th Edition
+ * Author of Book: Tony Gaddis and Godfrey MuGanda
+ * Page: 25
+ * Description: This Assignment will help you get acquainted with your Java development software. 
+ * 				Here is the Java program you will enter:
+*/
+
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
